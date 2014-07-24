4b2a7c82233a4e760efdfcc1ccdcc8c69c168c0f
Cata <beyond_k7a@yahoo.com>
2014-07-24 20:46:35 +0300

    fixed some menu links related issues, navbar now gets back up if small viewport and link clicked

--- js/dynamic_loading.js
+++ js/dynamic_loading.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
     var newHash = "",
         $mainContent = $("#main_content"),
@@ -9,24 +9,26 @@ $(function() {
     $pageWrap.height($pageWrap.height());
     baseHeight = $pageWrap.height() - $mainContent.height();
 
-    $("nav.navbar").delegate("a", "click", function() {
+    $("nav.navbar").delegate("a", "click", function () {
         window.location.hash = $(this).attr("href");
         return false;
     });
 
-    $(window).bind('hashchange', function() {
+    $(window).bind('hashchange', function () {
 
         newHash = window.location.hash.substring(1);
 
         if (newHash) {
+            //  Hide the collapsed navbar if link is clicked
+            if ($("div.navbar-collapse.navHeaderCollapse.collapse.in").length)
+                $("nav.navbar-fixed-top button").click();
             $mainContent
-                
-                .fadeOut(200, function() {
-                    $mainContent.hide().load(newHash + " #main_content", function() {
-                        $mainContent.fadeIn(200, function() {
+                .fadeOut(200, function () {
+                    $mainContent.hide().load(newHash + " #main_content", function () {
+                        $mainContent.fadeIn(200, function () {
+                            // ???
                         });
                         $("nav li").removeClass("active");
-
                         $("nav a[href='" + newHash + "']").parent().addClass("active");
                     });
                 });
